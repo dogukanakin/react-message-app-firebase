a2caa526e56130baac6460e4164234f144a7d5de
Doğukan Akın <dogukanakin@192.168.1.53>
2023-05-29 18:36:15 +0300

    das

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,4 @@
 import React from 'react'
-import Signin from './Signin'
-import Logout from './Logout'
-import { auth } from '../firebase'
 import { useAuthState } from 'react-firebase-hooks/auth'
 const style = {
   nav: `bg-gray-800 h-20 flex justify-between items-center p-4`,
@@ -9,12 +6,9 @@ const style = {
 }
 
 const Navbar = () => {
-  const [user] = useAuthState(auth)
-  console.log(user)
   return (
     <div className={style.nav}>
       <h1 className={style.heading}>Chat App</h1>
-      {user ? <Logout /> : <Signin />}
     </div>
   )
 }
